61db24b3a31a89b850e8a672336f69ccbc170c35
Juan Muñoz <128206390+JuanNucho@users.noreply.github.com>
2026-03-19 22:48:20 -0600

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,27 @@
+const quizQuestions = [
+{
+question: "Which branch of government makes laws?",
+answers:["Executive","Judicial","Legislative","State"],
+correct:2
+},
+{
+question:"The New Deal was created in response to what?",
+answers:["Civil War","Great Depression","Cold War","WWII"],
+correct:1
+},
+{
+question:"Inflation means:",
+answers:["Prices rise","Prices fall","Taxes drop","Jobs increase"],
+correct:0
+},
+{
+question:"‘We the People’ is from the:",
+answers:["Bill of Rights","Preamble","Gettysburg Address","Amendment 10"],
+correct:1
+},
+{
+question:"Political cartoons use symbols to:",
+answers:["Confuse","Entertain","Make a political point","Sell products"],
+correct:2
+}
+];
